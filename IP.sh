c1e3cfd1cc47ba99779b3f1d9c15480d6d7bc554
KrakeNocto <octo2kraken@gmail.com>
2024-08-13 13:33:34 +0300

    Update IP.sh

--- IP.sh
+++ IP.sh
@@ -1,4 +1,6 @@
 ip=$(curl -s https://2ip.ru)
 sed -ie "s|https://rpc-testnet.0g.ai|http://$ip:8545|" /etc/systemd/system/daclient.service
+sed -ie "s|https://rpc-testnet.0g.ai|http://$ip:8545|" /root/0g-da-node/config.toml
+
 systemctl daemon-reload
-systemctl restart daclient && journalctl -fu daclient
+systemctl restart da daclient
